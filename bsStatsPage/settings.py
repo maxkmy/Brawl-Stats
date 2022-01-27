40fc1065cf656c9bf1b037f0ac78beb3f0ab11eb
maxkmy <75100645+maxkmy@users.noreply.github.com>
2022-01-27 14:21:54 -0500

    Remove Django Secret Key

--- bsStatsPage/settings.py
+++ bsStatsPage/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-38zhjiiv@_*iz!f&66295sivoaa4+bn$g1z&wkyn+$m)u7#d#4'
+SECRET_KEY = your_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
